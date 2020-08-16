9215fdb55515f3a25b36a7a168c916967ddf74c4
sadavarterohit <sadavarte.rohit851@gmail.com>
2020-08-16 17:12:15 +0530

    Index issue

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,10 +8,10 @@ const schoolRoutes = require('./school')
 // const inquilabRoutes = require('./inquilab')
 // const studentRoutes = require('./student')
 // const teamRoutes = require('./team')
-const classObservationRoutes = require('./classObservation')
+// const classObservationRoutes = require('./classObservation')
 // const unitEvalutationRoutes = require('./unitEvalutation')
 // const teacherRoutes = require('./teacher')
-const studentTeacher = require('./studentTeacher')
+// const studentTeacher = require('./studentTeacher')
 
 //Combine routes
 routes.use(userRoutes)
@@ -20,10 +20,10 @@ routes.use(schoolRoutes)
 // routes.use(inquilabRoutes)
 // routes.use(studentRoutes)
 // routes.use(teamRoutes)
- routes.use(classObservationRoutes)
+// routes.use(classObservationRoutes)
 // routes.use(unitEvalutationRoutes)
 // routes.use(teacherRoutes)
-routes.use(studentTeacher)
+// routes.use(studentTeacher)
 
 
 
