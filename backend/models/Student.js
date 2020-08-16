cebba293d308c3326356b8dd6bde4658f0c46482
sadavarterohit <sadavarte.rohit851@gmail.com>
2020-08-16 12:31:41 +0530

    Added Student Schema

---
+++ backend/models/Student.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const { truncate } = require('fs');
+const Schema = mongoose.Schema;
+
+const StudentSchema = new Schema({
+    name : {
+        type : String,
+        required : true,
+    },
+    
+    roll : {
+        type : String,
+        required : true,
+    },
+
+    team : {
+        type : Number, 
+        required : true,
+    },
+
+    teacher: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref:'teachers',
+        
+    },
+
+    /*
+    class : {
+        type : Number,
+        required : true,
+    },
+*/
+    enabled: {
+        type:Boolean,
+        default: true,
+        required: false
+    },
+
+});
+
+module.exports = Student = mongoose.model("Student", StudentSchema)
